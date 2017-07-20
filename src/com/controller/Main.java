53560be3a321099ad690dc6cef722c55697f4a6d
engeloac <engeloac@aol.com>
2017-07-20 17:04:55 -0600

    .Se elimino codigo comentado -com.controller.Main.java-

--- src/com/controller/Main.java
+++ src/com/controller/Main.java
@@ -15,14 +15,5 @@ public class Main {
     }
     public static void main(String[] args) {
         new Main();
-//        com.view.MainWindow mainWindow = new com.view.MainWindow();
-//        com.controller.Controller controller = new com.controller.Controller();
-//        com.model.Model model = new com.model.Model();
-//        
-//        mainWindow.setController(controller);
-//        model.setController(controller);
-//        
-//        controller.setMainWindow(mainWindow);
-//        controller.setModel(model);
     }
 }
